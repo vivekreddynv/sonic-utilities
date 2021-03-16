564bbdfc7b5f8a9ce6d2835e05f75dc20d70f0c4
Vivek Reddy Karri <vkarri@nvidia.com>
2021-03-16 18:53:30 +0000

    Refactoring
    Signed-off-by: Vivek Reddy Karri <vkarri@nvidia.com>

--- debug/main.py
+++ debug/main.py
@@ -1,6 +1,8 @@
 import click
 import subprocess
 
+from . import dump_ports
+from .copp import copp as copp_cmd
 
 def run_command(command, pager=False):
     click.echo(click.style("Command: ", fg='cyan') + click.style(command, fg='green'))
@@ -10,14 +12,6 @@ def run_command(command, pager=False):
         click.echo_via_pager(output)
     else:
         click.echo(output)
-
-from . import dump_ports
-
-
-from .copp import copp as copp_cmd
-
-
-
 
 
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help', '-?'])
